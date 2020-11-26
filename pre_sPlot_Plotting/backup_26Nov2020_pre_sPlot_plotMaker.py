fec87f6c93eb89ab5113381943430c8a71772d2f
Mary Hadley <mary.hadley@cern.ch>
2020-11-26 16:56:19 +0100

    pre_sPlot_plotMaker.py: made preliminary attempt at customizing binning based on what var is being plotted. backup_26Nov2020_pre_sPlot_plotMaker.py: kept the old working version with dummy binning for all plots around in case I ever need to revert to it one day

--- pre_sPlot_Plotting/backup_26Nov2020_pre_sPlot_plotMaker.py
+++ pre_sPlot_Plotting/backup_26Nov2020_pre_sPlot_plotMaker.py
@@ -54,35 +54,8 @@ for sample in sampleDict.keys():
     for var in varNames:
         histName = "h_%s_%s" %(var, sample)
         print 'histName:', histName
-        
-        if 'upsi_mass' in histName:
-            myHists.append(ROOT.TH1F(histName, histName, 100, 7, 12))
-            myChain.Draw("%s>> %s"%(var, histName))
-            
-        if 'Z_mass' in histName:
-            myHists.append(ROOT.TH1F(histName, histName, 120, 60, 120))
-            myChain.Draw("%s>> %s"%(var, histName))
-        
-        #These should be empty right now because they are all set to -99 and I haven't filled them yet 
-        if 'pT_mu_from_Z_pT' in histName:
-            myHists.append(ROOT.TH1F(histName, histName, 100, 0, 200))
-            myChain.Draw("%s>> %s"%(var, histName))
-        
-        if 'pT_mu_from_upsi_pT' in histName:
-            myHists.append(ROOT.TH1F(histName, histName, 20, 0, 20))
-            myChain.Draw("%s>> %s"%(var, histName))
-            
-        if 'eta' in histName:
-            myHists.append(ROOT.TH1F(histName, histName, 20, -2.5, 2.5))   
-            myChain.Draw("%s>> %s"%(var, histName))
-            
-        if 'RAPIDITY' in histName:
-            myHists.append(ROOT.TH1F(histName, histName, 20, -2.5, 2.5)) 
-            myChain.Draw("%s>> %s" %(var, histName))
-            
-        else:
-            myHists.append(ROOT.TH1F(histName, histName, 600, -100, 200)) #dummy  binning FIX ME
-            myChain.Draw("%s>> %s"%(var, histName))
+        myHists.append(ROOT.TH1F(histName, histName, 600, -100, 200)) #dummy  binning FIX ME
+        myChain.Draw("%s>> %s"%(var, histName))
         
 
 print "myHists: \n"
