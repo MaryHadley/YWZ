a61ddb1ec4fcf9200faa29a87245eb7ee01a1318
Mary Hadley <mary.hadley@cern.ch>
2021-05-14 18:36:28 +0200

    "groom" the initial output lhe files for all color octet initial output files and/or all Y (2 or 3S) output lhe files to prepare them for combination
    As Hua-Sheng explained, color octet states and Y(2S) and Y(3S) output lhe files need to be "groomed" with the input/py8_onia_user.inp and the ho_dir/bin/lhe_pythia6_pythia8 files. These bash scripts currently only consider the Y2S3S11 and Y3S3S11 states. They also have yet to be tested. They should, therefore, be considered rough drafts that are here for safe keeping

---
+++ SPS_MC_Creation/groom_Y3S_fockStates.sh
@@ -0,0 +1,30 @@
+#Run me from ho_dir, the main Helac-Onia directory 
+ho_dir = /afs/cern.ch/work/m/mhadley/public/GOOD_HELAC-Onia_MinimalSetup/HELAC-Onia-2.5.5
+runDir =FIXME
+
+echo HAVE YOU MADE THE APPROPRIATE CHANGE TO THE FIRST ONIA PDGID LINE IN input/py8_onia_user.inp? 
+echo REMEMBER YOU ARE RUNNING Y 3S SAMPLES NOW! 
+echo CONFIRM YOU HAVE MADE THE CHANGE BY ENTERING YES OR yes or Yes or Y or y
+
+read yesorno
+
+if [ "$yesorno" = yes ] || [ "$yesorno" = YES ] || [ "$yesorno" = Yes ]  || [ "$yesorno" = Y ]  || [ "$yesorno" = y ]
+
+then
+    echo PROCEEDING WITH GROOMING...
+    
+    echo GROOMING Y3S3S11 SAMPLES...
+    #Y233S11 samples 
+    for i in 0 1 2 3 4 5 6 7 8
+    do
+       cp input/py8_onia_user.inp $runDir/PROC_HO_0/P0_calc_$i/output
+       $ho_dir/bin/lhe_pythia6_pythia8
+       cd ho_dir
+    done
+    
+        
+else
+    echo EXITING SO YOU CAN FIX THE input/py8_onia_user.inp...
+    echo GOODBYE!
+
+fi
